fb15b2a149d1e8054647ab833e3ad43003d1b8f3
Sasha Golovin <a.golovin@innopolis.university>
2023-02-20 21:55:46 +0300

    Add files via upload

--- script/script.js
+++ script/script.js
@@ -1,11 +1,11 @@
 const header = document.querySelector('.navbar');
 
 window.onscroll = function() {
-    var top = window.scrollY;
-    if(top >=100) {
-        header.classList.add('navbarDark');
-    }
-    else {
-        header.classList.remove('navbarDark');
+     var top = window.scrollY;
+     if(top >=100) {
+         header.classList.add('navbarDark');
+       }
+      else {
+          header.classList.remove('navbarDark');
     }
 }
